6f1e1fff9994c59ee945adeabebe7b2bffff649d
ahmednasserg <ahmednasser217217@gmail.com>
2022-02-03 20:34:16 +0200

    Add: contact route
    refactored app.js to extract contact route

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const lodash = require('lodash');
 
-const contactContent =
-  'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,13 +12,11 @@ app.use(express.static('public'));
 
 const homeRoute = require('./routes/home');
 const aboutRoute = require('./routes/about');
+const contactRoute = require('./routes/contact');
 
 app.use('/', homeRoute);
 app.use('/about', aboutRoute);
-
-app.get('/contact', function (req, res) {
-  res.render('contact', { contactContent: contactContent });
-});
+app.use('/contact', contactRoute);
 
 app.get('/compose', function (req, res) {
   res.render('compose');
