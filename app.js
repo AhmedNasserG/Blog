4a43268a056d908c591e7084c5aa2d759db17752
ahmednasserg <ahmednasser217217@gmail.com>
2022-02-03 20:25:19 +0200

    Add: home route
    refactored app.js to extract home route

--- app.js
+++ app.js
@@ -1,65 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const lodash = require('lodash');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const lodash = require("lodash");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const aboutContent =
+  'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.';
+const contactContent =
+  'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
 const app = express();
 
-let posts = [];
-
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
+const homeRoute = require('./routes/home');
 
-app.get("/", function(req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts,
-  });
+app.use('/', homeRoute);
+
+app.get('/about', function (req, res) {
+  res.render('about', { aboutContent: aboutContent });
 });
 
-app.get("/about", function(req, res) {
-  res.render("about", {aboutContent: aboutContent});
+app.get('/contact', function (req, res) {
+  res.render('contact', { contactContent: contactContent });
 });
 
-app.get("/contact", function(req, res) {
-  res.render("contact", {contactContent: contactContent});
+app.get('/compose', function (req, res) {
+  res.render('compose');
 });
 
-app.get("/compose", function(req, res) {
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res) {
+app.post('/compose', function (req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
   };
   posts.push(post);
-  res.redirect("/");
+  res.redirect('/');
 });
 
-app.get("/posts/:postTitle", function(req, res) {
+app.get('/posts/:postTitle', function (req, res) {
   const requestedPostTitle = req.params.postTitle;
   const post = getPost(requestedPostTitle);
   if (post !== null) {
-    res.render("post", {post: post});
+    res.render('post', { post: post });
   } else {
-    res.redirect("/");
+    res.redirect('/');
   }
 });
 
 function getPost(title) {
   title = lodash.lowerCase(title);
   var post = null;
-  posts.forEach(function(item) {
+  posts.forEach(function (item) {
     var itemTitle = lodash.lowerCase(item.title);
     if (title === itemTitle) {
       post = item;
@@ -68,6 +62,6 @@ function getPost(title) {
   return post;
 }
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function () {
+  console.log('Server started on port 3000');
 });
