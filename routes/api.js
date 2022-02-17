b99ff730229bfa4fbf51fa0b1d95d127e5362e62
AhmedNasserG <ahmednasser217217@gmail.com>
2022-02-17 23:58:45 +0200

    Update api
    implement get, put, patch, and delete for '/api/posts/:postId'

--- routes/api.js
+++ routes/api.js
@@ -33,4 +33,47 @@ router
 		});
 	});
 
+router
+	.route('/posts/:postId')
+	.get((req, res) => {
+		Post.findOne({ _id: req.params.postId }, (err, foundPost) => {
+			if (!err) {
+				res.send(foundPost);
+			} else {
+				res.send(err);
+			}
+		});
+	})
+	.put((req, res) => {
+		Post.updateOne(
+			{ _id: req.params.postId },
+			{ title: req.body.title, content: req.body.content },
+			(err) => {
+				if (!err) {
+					res.send('the post has been updated');
+				} else {
+					res.send(err);
+				}
+			}
+		);
+	})
+	.patch((req, res) => {
+		Post.updateOne({ _id: req.params.postId }, { $set: req.body }, (err) => {
+			if (!err) {
+				res.send('the post has been updated');
+			} else {
+				res.send(err);
+			}
+		});
+	})
+	.delete((req, res) => {
+		Post.deleteOne({ _id: req.params.postId }, (err) => {
+			if (!err) {
+				res.send('the post has been deleted');
+			} else {
+				res.send(err);
+			}
+		});
+	});
+
 module.exports = router;
